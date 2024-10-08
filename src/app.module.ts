1350ba761667c532013abc72d139f6ac7a4e0f2a
onier9702 <onier0217@gmail.com>
2024-10-08 16:08:31 -0600

    [update] health-check module to allow tetsing pods from goggle kubernetes engine added as requirement from google documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+
+import { HealthCheckModule } from './health-check/health-check.module';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [PaymentModule],
+  imports: [
+    PaymentModule,
+    HealthCheckModule,
+  ],
   controllers: [],
   providers: [],
 })
